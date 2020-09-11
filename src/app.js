92be577a7228115a97753cde09e08384cf6a7168
mateusamarall <mateus.amaral018@gmail.com>
2020-09-10 22:48:45 -0300

    rota para listar usuarios

--- src/app.js
+++ src/app.js
@@ -1,7 +1,18 @@
 const express = require('express');
+
 const app = express();
 app.get('/', (req, res) => {
-    res.status(200).send();
+  res.status(200).send();
+});
+
+app.get('/users', (req, res) => {
+  const users = [
+    {
+      name: 'mateus', email: 'mateus@gmail.com',
+    },
+  ];
+
+  res.status(200).json(users);
 });
 
 module.exports = app;
