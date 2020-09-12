a281d98a7dacc844f0a6a1bc1fb191ff54e5b0c6
mateusamarall <mateus.amaral018@gmail.com>
2020-09-12 16:59:51 -0300

    adicionado rota apra criação de usuarios e passando pro express entender json

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 
 const app = express();
+
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.status(200).send();
 });
@@ -13,6 +16,10 @@ app.get('/users', (req, res) => {
   ];
 
   res.status(200).json(users);
+});
+
+app.post('/users', (req, res) => {
+  res.status(201).json(req.body);
 });
 
 module.exports = app;
